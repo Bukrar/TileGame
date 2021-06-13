d0f038df55b5ec972a7114e23f3eabee740aff34
Bukrar <alex968624229@gmail.com>
2021-06-14 00:00:50 +0800

    腳色跑步和轉身

--- TileGame/Assets/Scripts/Player.cs
+++ TileGame/Assets/Scripts/Player.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+
+    bool isAlive = true;
+
+
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Run();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
+        float controlThrow = Input.GetAxis("Horizontal");
+        Vector2 playervelocity = new Vector2(controlThrow * 5f, myRigidbody.velocity.y);
+        myRigidbody.velocity = playervelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            //transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+            transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x) * Mathf.Sign(myRigidbody.velocity.x), transform.localScale.y);
+        }
     }
 }
